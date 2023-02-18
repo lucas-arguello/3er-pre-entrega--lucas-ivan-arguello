e73377923637469c71e1b6b68dd5a9edd2726db8
Lucas Arguello <lucas5ivan@hotmail.com>
2023-02-18 11:12:41 -0300

    inicio de 3er proyecto

---
+++ app.js
@@ -0,0 +1,124 @@
+//1
+const contenedorPlantas = document.getElementById("contenedor-plantas");
+//2
+const contenedorCarrito = document.getElementById("carrito-contenedor");
+
+//7 Actualizar el precio total y luego agrego el contador en el actualizador del carrito.
+
+const precioTotal = document.getElementById("precioTotal")
+
+
+//2
+let carrito = [];
+
+document.addEventListener(`DOMContentLoaded`, ( ) => {
+    if (localStorage.getItem(`carrito`)){
+        carrito = JSON.parse(localStorage.getItem(`carrito`))
+        actualizCarrito()
+    }
+})
+
+//1 
+grupoPlantas.forEach((plantas) => {
+    const div = document.createElement('div')
+    div.classList.add('plantas')
+    div.innerHTML = `
+        <img src=${plantas.img} alt= "">
+        <h3>${plantas.categoria} ${plantas.nombre}</h3>
+        <p></p>
+        <p>${plantas.descr}</p>
+        <p class="precioProducto">Precio:$ ${plantas.precio}</p>
+        <button id="agregar${plantas.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
+    `
+    contenedorPlantas.appendChild(div)
+
+    //3
+    const btnElim = document.getElementById(`agregar${plantas.id}`)
+    btnElim.addEventListener("click", () => {
+        agregarAlCarrito(plantas.id)
+    })
+})
+
+
+//2
+const agregarAlCarrito = (prodId) =>{
+    //comprobamos si el tipo de planta ya existe 
+    const existe = carrito.some(prod => prod.id === prodId)
+
+    if (existe){
+        const prod = carrito.map ( prod => {
+            if (prod.id === prodId){
+                prod.cantidad++
+            }
+        })
+
+    } else {
+        //el id del array coincida con el id que llega por parametro
+    const item = grupoPlantas.find((prod) => prod.id === prodId)
+    carrito.push(item)
+    console.log(carrito)
+
+    }
+    
+    actualizCarrito()
+    
+}
+
+
+//3
+const actualizCarrito = () =>{
+
+    // actualizo el carrito
+    contenedorCarrito.innerHTML = "";
+    carrito.forEach((prod) => {
+        const div = document.createElement("div");
+        div.classList.add("productoEnCarrito")
+        div.innerHTML = `
+        <p>${prod.categoria}</p>
+        <p>Precic: ${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button id="eliminarDelCarrito${prod.id}" class ="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+        contenedorCarrito.appendChild(div)
+
+        localStorage.setItem(`carrito`, JSON.stringify(carrito))
+
+        //4 boton vaciar carrito 
+        const btnElimDelCarrito = document.getElementById(`eliminarDelCarrito${prod.id}`)
+        btnElimDelCarrito.addEventListener("click", () => {
+            eliminarDelCarrito(prod.id)
+
+    
+        })
+    contadorCarrito.innerText = carrito.length
+
+    precioTotal.innerText = carrito.reduce((acum, prod) => acum + prod.precio, 0 )
+    })
+}
+
+//6
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+    const indice = carrito.indexOf(item)
+    // encuentra el indice y borra uno
+    carrito.splice(indice, 1)
+    //pinto el carrito 
+    actualizCarrito()
+}
+
+//Boton vaciar carrito
+
+const botonVaciar = document.getElementById("vaciar-carrito");
+
+botonVaciar.addEventListener("click", () =>{
+    carrito.length = 0
+    actualizCarrito()
+})
+
+//5 Contador del carrito- accedo y luego agrego el contador en el actualizador del carrito.
+const contadorCarrito = document.getElementById("contadorCarrito");
+
+
+
+
+
---
+++ stock.js
@@ -0,0 +1,30 @@
+class Planta {
+    constructor(id, categoria, nombre, cantidad, descr, precio,img ){
+        this.id = id
+        this.categoria = categoria
+        this.nombre = nombre
+        this.cantidad = cantidad
+        this.descr = descr
+        this.precio = precio
+        this.img = img
+    }
+}
+
+let grupoPlantas = [
+    new Planta (1, "Planta de interior", "- Aechemea", 1, "Es una bromeliade de hojas perennes, arrosetadas con margenes armados.", 1000, "./img/Aechemea01.jpg" ),
+    new Planta (2, "Planta de interiorr", "- Anthurium", 1, "Es caduca, herbáceas, de hojas decorativas, gruesas y ovales. ", 1500, "./img/Anthurium02.jpg" ),
+    new Planta (3, "Planta de interior", "- Ixora o cruz de Malta", 1, "Arbusto perenne con hojas brillantes, textura similar al cuero.", 2000, "./img/Ixora03.jpg" ),
+    new Planta (4, "Planta de interior", "- Dendrobium", 1, "De la familia de las orquiedeas, de hojas muy grandes.", 1500, "./img/Dendrobium04.jpg" ),
+    new Planta (5, "Planta de interior", "- Guzmania", 1, "Es una Acaulescente de hojas color naranja con bracteas rojas, planta polistica. ", 1000, "./img/Guzmania05.jpg" ),
+    new Planta (6, "Planta de interior", "- Orquidea lila", 1, "Es de hojas perennes de color muy vibrante y suaves, con tallos cortos.", 1200, "./img/Orquidea06.jpg" ),
+    new Planta (7, "Planta de exterior", "- Alelí o Matthiola Incana", 1, "Es un ahebacea bienal o perenne, su tallo suele medir 15 cm, de hojas suaves.", 1000, "./img/Aleli07.jpg" ),
+    new Planta (8, "Planta de exterior", "- Copete", 1, "Es una planta anual, sus hojas son de mucho follaje, y polinizadoras.", 1500, "./img/copete08.jpg" ),
+    new Planta (9, "Planta de exterior", "- Euriops", 1, "Es un arbusto de hojas perennes, crece hasta 1.5 mts, de hojas suaves y peludas.", 1300, "./img/Euriops09.jpg" ),
+    new Planta (10, "Planta de exterior", "- Lantana", 1, "Es de hojas opuestas, ovales, dentadas, ásperas, crece hasta 2 mts.", 1300, "./img/Lantana010.jpg" ),
+    new Planta (11, "Planta de exterior", "- Oxalis o Vinagrera", 1, "Es de hojas que están divididas en tres a diez folíolos acorazonados o lanceolados.", 1300, "./img/Oxalis011.jpg" ),
+    new Planta (12, "Planta de exterior", "- Rayito de Sol", 1, "Es una herbacea perenne de hojas triangulares con flores violetas, rosas y lilas.", 1300, "./img/Rayito-de-sol012.jpg" )
+    
+]
+
+
+
